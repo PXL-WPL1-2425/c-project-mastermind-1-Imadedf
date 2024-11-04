021bf35237350faf45e43c691829c7582af8a461
Imad Ed-daïf <12401653@student.pxl.be>
2024-11-04 15:31:35 +0100

    Mastermind-01

---
+++ Project-Mastermind-1/Project-Mastermind-1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project_MasterMind_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string[] colors = { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComboBoxes();
+        }
+        private void InitializeComboBoxes()
+        {
+            foreach (var color in colors)
+            {
+                ComboBox1.Items.Add(color);
+                ComboBox2.Items.Add(color);
+                ComboBox3.Items.Add(color);
+                ComboBox4.Items.Add(color);
+            }
+        }
+
+        private void ValidateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Placeholder for validation logic
+        }
+    }
+
+
+
+}
