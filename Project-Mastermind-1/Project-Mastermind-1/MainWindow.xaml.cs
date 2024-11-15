157aa1c25ca02cc5b7ca90c38d0e4ef9490af8de
Imad Ed-daïf <12401653@student.pxl.be>
2024-11-15 22:38:51 +0100

    MasterMind-04 Kleur kiezen

--- Project-Mastermind-1/Project-Mastermind-1/MainWindow.xaml.cs
+++ Project-Mastermind-1/Project-Mastermind-1/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Reflection.Emit;
+
 
 namespace project_MasterMind_1
 {
@@ -23,13 +25,13 @@ namespace project_MasterMind_1
 
         private void GenerateRandomCode()
         {
-            Random random = new Random();  
+            Random random = new Random();
             for (int i = 0; i < generatedCode.Length; i++)
             {
-                generatedCode[i] = colors[random.Next(colors.Length)];  
+                generatedCode[i] = colors[random.Next(colors.Length)];
             }
             // Toon de code in het titelvenster voor testdoeleinden
-            this.Title = $"Mastermind - Geheime Code: {string.Join(", ", generatedCode)}";  
+            this.Title = $"Mastermind - Geheime Code: {string.Join(", ", generatedCode)}";
 
 
 
@@ -55,8 +57,46 @@ namespace project_MasterMind_1
         {
             // Placeholder for validation logic
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is not ComboBox comboBox)
+                return;
+
+            // Verkrijg de geselecteerde waarde als string
+            string selectedColor = comboBox.SelectedItem as string;
+
+            // Label opzoeken
+            System.Windows.Controls.Label correspondingLabel = comboBox.Name switch
+            {
+                "ComboBox1" => Label1,
+                "ComboBox2" => Label2,
+                "ComboBox3" => Label3,
+                "ComboBox4" => Label4,
+                _ => null
+            };
+
+            if (correspondingLabel != null)
+            {
+                if (!string.IsNullOrEmpty(selectedColor))
+                {
+                    // Zet de achtergrondkleur van het Label
+                    correspondingLabel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(selectedColor));
+                    correspondingLabel.Content = ""; // Verwijder tekst
+                }
+                else
+                {
+                    // Reset naar standaard wanneer geen kleur is geselecteerd
+                    correspondingLabel.Background = Brushes.Transparent;
+                    correspondingLabel.Content = "Geen kleur gekozen";
+                }
+
+
+
+            }
+
+
+
+        }
     }
-
-    
-
 }
